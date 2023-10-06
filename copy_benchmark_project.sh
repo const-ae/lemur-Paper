2d93e262262fea2b2ac5f4594c78ddf1b0487e1f
const-ae <artjom31415@googlemail.com>
2023-10-06 13:11:30 +0200

    Initial commit

---
+++ copy_benchmark_project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove old folder content
+rm -r benchmark
+# Make new folder
+mkdir benchmark
+mkdir benchmark/renv
+
+# Copy relevant files (i.e., exclude renv library because it is too big)
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/{README.md,renv.lock,.Rprofile} benchmark/.
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/src benchmark/src
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/renv/activate.R benchmark/renv/activate.R
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/output benchmark/output
