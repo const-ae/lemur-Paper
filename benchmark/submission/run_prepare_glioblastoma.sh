abc04a5105c6b612774d96f03e7b8dcd9f9e2753
const-ae <artjom31415@googlemail.com>
2023-12-13 13:50:58 +0100

    Update outfile for run_prepare*.sh scripts

--- benchmark/submission/run_prepare_glioblastoma.sh
+++ benchmark/submission/run_prepare_glioblastoma.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=120GB
 #SBATCH --job-name=render_prepare_glioblastoma
 #SBATCH --output=tmp/slurm_logs/log-%x.%j.out
-#SBATCH --output=tmp/slurm_logs/log-%x.%j.err
+#SBATCH --error=tmp/slurm_logs/log-%x.%j.err
   
 module load Pandoc/2.19
 module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
