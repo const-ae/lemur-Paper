88911850ccc4b435ed86d199c8497eef48ffadc3
const-ae <artjom31415@googlemail.com>
2024-01-09 17:16:36 +0100

    Add 'render_notebooks.sh' script

---
+++ render_notebooks.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+Rscript -e "rmarkdown::render('notebooks/benchmark_results.Rmd', output_format = 'html_document')"
+Rscript -e "rmarkdown::render('notebooks/alzheimer_plaques_analysis.Rmd', output_format = 'html_document')"
+Rscript -e "rmarkdown::render('notebooks/zebrafish_analysis.Rmd', output_format = 'html_document')"
+Rscript -e "rmarkdown::render('notebooks/glioblastoma_analysis.Rmd', output_format = 'html_document')"
