d816b162eb0f449fa3ccc19e2bcc92c5fc63cd24
const-ae <artjom31415@googlemail.com>
2024-03-20 16:00:47 +0100

    Add notebook with simulation study

--- render_notebooks.sh
+++ render_notebooks.sh
@@ -4,3 +4,4 @@ Rscript -e "rmarkdown::render('notebooks/benchmark_results.Rmd', output_format =
 Rscript -e "rmarkdown::render('notebooks/alzheimer_plaques_analysis.Rmd', output_format = 'html_document')"
 Rscript -e "rmarkdown::render('notebooks/zebrafish_analysis.Rmd', output_format = 'html_document')"
 Rscript -e "rmarkdown::render('notebooks/glioblastoma_analysis.Rmd', output_format = 'html_document')"
+Rscript -e "rmarkdown::render('notebooks/simulation_study.Rmd', output_format = 'html_document')"
