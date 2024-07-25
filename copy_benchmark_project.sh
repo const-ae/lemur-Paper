2fb0d124b9fb3214f7983f83b33cec903c0add45
const-ae <artjom31415@googlemail.com>
2024-07-25 13:21:51 +0200

    Update copy_benchmark_project script

--- copy_benchmark_project.sh
+++ copy_benchmark_project.sh
@@ -8,9 +8,10 @@ mkdir benchmark/renv
 mkdir benchmark/data
 
 # Copy relevant files (i.e., exclude renv library because it is too big)
-scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/{README.md,renv.lock,.Rprofile,run_benchmarks.sh,conda_env_package_versions.txt} benchmark/.
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/{README.md,renv.lock,.Rprofile,run_benchmarks.sh} benchmark/.
 scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/renv/activate.R benchmark/renv/activate.R
 scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/submission benchmark/submission
 scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/src benchmark/src
 scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/output benchmark/output
+scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/conda_env_info benchmark/conda_env_info
 scp -r ahlmanne@seneca:~/projects/lemur-Paper-benchmark/data/{de_data_spec.tsv,datasets.yaml,.gitignore} benchmark/data/.
